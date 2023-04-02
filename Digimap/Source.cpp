f1fe72131d8b5478c0411dda245ce76db95faf37
josephogren <joseph.ar.ogren@outlook.com>
2023-04-02 22:15:28 +0200

    cornerHarris

--- Digimap/Source.cpp
+++ Digimap/Source.cpp
@@ -1,21 +1,34 @@
+#include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include "opencv2/imgproc.hpp"
 #include <iostream>
 using namespace cv;
 using namespace std;
 int main(int argc, char** argv)
 {   
     std::cout << "DigiMap v1.0" << std::endl;
-    Mat image;
-    image = imread("C:\\Dev\\Repo\\cpp\\Digimap\\x64\\Debug\\opencv-logo.png", IMREAD_COLOR); // Read the file
-    if (image.empty()) // Check for invalid input
+
+    Mat input_image = imread("C:\\Dev\\Repo\\cpp\\Digimap\\x64\\Debug\\input_image.jpg", IMREAD_GRAYSCALE);
+
+    if (input_image.empty()) // Check for invalid input
     {
         cout << "Could not open or find the image" << std::endl;
-        return -1; 
+        return -1;
     }
-    namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-    imshow("Display window", image); // Show our image inside it.
-    waitKey(0); // Wait for a keystroke in the window
+
+    // Detect corners using Harris corner detector
+    Mat corner_image;
+    cornerHarris(input_image, corner_image, 2, 3, 0.04);
+
+    // Normalize corner response image for display
+    Mat corner_norm;
+    normalize(corner_image, corner_norm, 0, 255, NORM_MINMAX, CV_8UC1);
+
+    // Display corner response image
+    imshow("Corner Response Image", corner_norm);
+    waitKey(0);
+
     return 0;
 }
